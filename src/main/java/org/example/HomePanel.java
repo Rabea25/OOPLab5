6677d22d3e5d5710691510eb6f2051b029ea89f2
bdbd05 <abdelrahmanahmed20052005@gmail.com>
2025-10-25 14:56:12 +0300

    home panel and form updated and linked

--- src/main/java/org/example/HomePanel.java
+++ src/main/java/org/example/HomePanel.java
@@ -1,23 +1,18 @@
 package org.example;
-import com.intellij.uiDesigner.core.GridConstraints;
-import com.intellij.uiDesigner.core.GridLayoutManager;
 
 import javax.swing.*;
-import javax.swing.plaf.FontUIResource;
-import javax.swing.text.StyleContext;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.Locale;
 
 public class HomePanel extends JFrame {
     private JLabel title;
     private JButton homeButton;
     private JButton logoutButton;
-    private JButton editDeleteButton;
     private JButton viewButton;
-    private JButton addButton;
     private JButton searchButton;
+    private JButton addButton;
+    private JButton deleteButton;
     private JPanel contentPanel;
     private JPanel topPanel;
     private JTextArea welcomeField;
@@ -38,13 +33,7 @@ public class HomePanel extends JFrame {
         addButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                //switchPanel(AddPanel);
-            }
-        });
-        editDeleteButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                //switchPanel(EditDeletePanel);
+                //switchPanel( new AddPanel());
             }
         });
         viewButton.addActionListener(new ActionListener() {
@@ -56,13 +45,28 @@ public class HomePanel extends JFrame {
         searchButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                //switchPanel(SearchPanel);
+                //switchPanel(new EditPanel(db,));
+            }
+        });
+        deleteButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //switchPanel(new DeletePanel(db, ));
             }
         });
         logoutButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 JOptionPane.showMessageDialog(contentPanel, "Logging out....");
+                JFrame currentFrame = (JFrame) SwingUtilities.getWindowAncestor(contentPanel);
+                currentFrame.dispose();
+
+                JFrame loginFrame = new JFrame("Login");
+                loginFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                loginFrame.setSize(500, 400);
+                loginFrame.setLocationRelativeTo(null);
+                loginFrame.setContentPane(new LoginPanel().getRootPanel());
+                loginFrame.setVisible(true);
 
             }
         });
