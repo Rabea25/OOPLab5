9082bea20ee239f17996873f80a3da2678c14b42
Zeina Emad Hamza Mohamed Hussien <es.zina.husien2023@alexu.edu.eg>
2025-10-25 14:47:52 +0300

    created StudentDatabase class

---
+++ src/main/java/org/example/StudentDatabase.java
@@ -0,0 +1,115 @@
+package org.example;
+
+import java.io.*;
+import java.sql.Array;
+import java.util.*;
+import javax.swing.*;
+
+public class StudentDatabase {
+    private static final String filename = "Students.text";
+    private List<Student> students = new ArrayList<>();
+
+    public StudentDatabase()
+    {
+        loadStudents();
+    }
+
+    public void addStudent(Student s)
+    {
+        students.add(s);
+        saveStudents();
+        JOptionPane.showMessageDialog(null, "Student added successfully.", "Success", JOptionPane.INFORMATION_MESSAGE);
+    }
+
+    public void deleteStudent(int id)
+    {
+        for(int i = 0; i <students.size(); i++)
+        {
+            Student s = students.get(i);
+                if(Integer.parseInt(s.getId()) == id)
+                {
+                    students.remove(i);
+                    saveStudents();
+                    JOptionPane.showMessageDialog(null, "Student deleted successfully.", "Success", JOptionPane.INFORMATION_MESSAGE);
+                }
+        }
+        JOptionPane.showMessageDialog(null, "Student not found. ", "Error", JOptionPane.ERROR_MESSAGE);
+    }
+
+    public void updateStudent(Student updatedstudent)
+    {
+        for(Student s : students)
+        {
+            if(s.getId() == updatedstudent.getId())
+            {
+                s.setName(updatedstudent.getName());
+                s.setAge(updatedstudent.getAge());
+                s.setGender(updatedstudent.getGender());
+                s.setDepartment(updatedstudent.getDepartment());
+                s.setGpa(updatedstudent.getGpa());
+
+                saveStudents();
+                JOptionPane.showMessageDialog(null, "Student updated successfully!", "Success", JOptionPane.INFORMATION_MESSAGE);
+                return;
+            }
+        }
+    }
+
+
+    public List<Student> getStudents() { return new ArrayList<>(students); }
+
+
+    public Student findStudent(int id)
+    {
+        for (Student s : students) if(Integer.parseInt(s.getId()) == id) return s;
+        return null;
+    }
+
+
+    public List<Student> searchName(String name)
+    {
+        List<Student> result = new ArrayList<>();
+        for (Student s: students)
+        {
+            if(s.getName().toLowerCase().contains(name.toLowerCase())) result.add(s);
+        }
+        return result;
+    }
+
+    private void saveStudents()
+    {
+        try (BufferedWriter writes = new BufferedWriter(new FileWriter(filename)))
+        {
+            for (Student s : students)
+            {
+                writes.write(s.toFileString());
+                writes.newLine();
+            }
+        } catch(IOException e)
+        {
+            JOptionPane.showMessageDialog(null, "Error saving student.", "Error", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+    private void loadStudents()
+    {
+        students.clear();
+        try(BufferedReader reads = new BufferedReader((new FileReader(filename))))
+        {
+            String line;
+            while ((line = reads.readLine()) != null)
+                students.add(Student.fromFileString(line));
+        } catch (IOException e)
+        {
+            JOptionPane.showMessageDialog(null, "Error opening file. File might not exist.", "Error", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+
+
+
+
+
+
+
+}
